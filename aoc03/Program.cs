e3e5193fe3d18d46a69eb9562878b5673c23f996
Greg Tan <greg.tan@firstmode.com>
2022-12-04 22:32:23 +0800

    Day 3 part 2.

--- aoc03/Program.cs
+++ aoc03/Program.cs
@@ -1,16 +1,25 @@
-﻿public static class Program {
+﻿using System.Collections.Immutable;
+
+public static class Program {
+
+    private static ImmutableHashSet<char> AllContents = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray().ToImmutableHashSet();
+
     public static void Main(string[] args) {
         var rucksackContents = File.ReadLines(args[0])
             .Select(x => RucksackParser.CompartmentContents(x));
-
         var commonContents = rucksackContents
             .Select(x => CommonItemFinder.Find(x[0], x[1]));
-
         var priorities = commonContents
             .Select(x => x.Select(y => Prioritiser.Priority(y)).Sum());
-
         var allPrioritiesSum = priorities.Sum();
 
         Console.WriteLine($"Sum of common item priorities: {allPrioritiesSum}");
+
+        var groupContents = RucksackParser.GroupAndUniqueContents(File.ReadLines(args[0]), 3);
+        var badges = groupContents.Select(x => x.Aggregate((a, b) => a.Intersect(b)));
+        var badgePriorities = badges.Select(x => x.Select(y => Prioritiser.Priority(y)).Sum());
+        var badgePrioritiesSum = badgePriorities.Sum();
+
+        Console.WriteLine($"Sum of badge priorities: {badgePrioritiesSum}");
     }
 }
--- aoc03/RuckSackParser.cs
+++ aoc03/RuckSackParser.cs
@@ -8,4 +8,22 @@ public static class RucksackParser {
             .Select(x => x.ToCharArray().ToImmutableHashSet())
             .ToImmutableArray();
     }
+
+    public static IEnumerable<ImmutableArray<ImmutableHashSet<char>>> GroupAndUniqueContents(this IEnumerable<string> source, int groupSize) {
+        using (var enumerator = source.GetEnumerator()) {
+            var array = ImmutableArray<ImmutableHashSet<char>>.Empty;
+            while (enumerator.MoveNext()) {
+                var current = enumerator.Current.Trim();
+                var unique = current.ToCharArray().ToImmutableHashSet();
+                array = array.Add(unique);
+                if (array.Length == groupSize) {
+                    yield return array;
+                    array = ImmutableArray<ImmutableHashSet<char>>.Empty;
+                }
+            }
+            if (array.Length != 0) {
+                throw new ArgumentException($"Input length is not a multiple of {groupSize}");
+            }
+        }
+    }
 }
