89767e96f95318ebaf0cad50bf43aa91367fbe4d
Greg Tan <greg.tan@firstmode.com>
2022-12-05 18:20:39 +0800

    Day 5 part 2.

--- aoc05/Program.cs
+++ aoc05/Program.cs
@@ -3,9 +3,12 @@
 public class Program {
     public static void Main(string[] args) {
         var (initialState, moves) = Parser.Parse(args[0]);
-        var finalState = Rearranger.Rearrange(initialState, moves);
+        var finalStateCrateMover9000 = Rearranger.RearrangeCrateMover9000(initialState, moves);
+        var topOfStacksCrateMover9000 = string.Join("", finalStateCrateMover9000.Keys.Order().Select(k => finalStateCrateMover9000[k].Peek().ToString()));
+        Console.WriteLine($"Top of stacks, CrateMaster 9000: {topOfStacksCrateMover9000}");
 
-        var topOfStacks = string.Join("", finalState.Keys.Order().Select(k => finalState[k].Peek().ToString()));
-        Console.WriteLine($"Top of stacks: {topOfStacks}");
+        var finalStateCrateMover9001 = Rearranger.RearrangeCrateMover9001(initialState, moves);
+        var topOfStacksCrateMover9001 = string.Join("", finalStateCrateMover9001.Keys.Order().Select(k => finalStateCrateMover9001[k].Peek().ToString()));
+        Console.WriteLine($"Top of stacks, CrateMaster 9001: {topOfStacksCrateMover9001}");
     }
 }
--- aoc05/Rearranger.cs
+++ aoc05/Rearranger.cs
@@ -1,17 +1,41 @@
 using System.Collections.Immutable;
 
 public class Rearranger {
-    public static ImmutableDictionary<int, ImmutableStack<char>> Rearrange(ImmutableDictionary<int, ImmutableStack<char>> stacks, IEnumerable<(int Quantity, int From, int To)> moves) {
+    public static ImmutableDictionary<int, ImmutableStack<char>> RearrangeCrateMover9000(ImmutableDictionary<int, ImmutableStack<char>> stacks, IEnumerable<(int Quantity, int From, int To)> moves) {
         return moves.Expand()
-            .Aggregate(stacks, Move);
+            .Aggregate(stacks, MoveCrateMover9000);
     }
 
-    private static ImmutableDictionary<int, ImmutableStack<char>> Move(ImmutableDictionary<int, ImmutableStack<char>> stacks, (int From, int To) move) {
+    public static ImmutableDictionary<int, ImmutableStack<char>> RearrangeCrateMover9001(ImmutableDictionary<int, ImmutableStack<char>> stacks, IEnumerable<(int Quantity, int From, int To)> moves) {
+        return moves.Aggregate(stacks, MoveCrateMover9001);
+    }
+
+    private static ImmutableDictionary<int, ImmutableStack<char>> MoveCrateMover9000(ImmutableDictionary<int, ImmutableStack<char>> stacks, (int From, int To) move) {
         var fromStack = stacks[move.From];
         fromStack = fromStack.Pop(out var crate);
 
         var toStack = stacks[move.To];
         toStack = toStack.Push(crate);
+
+        return stacks
+            .SetItem(move.From, fromStack)
+            .SetItem(move.To, toStack);
+    }
+
+    private static ImmutableDictionary<int, ImmutableStack<char>> MoveCrateMover9001(ImmutableDictionary<int, ImmutableStack<char>> stacks, (int Quantity, int From, int To) move) {
+        var fromStack = stacks[move.From];
+
+        var tempStack = new Stack<char>();
+        for (var i = 0; i < move.Quantity; i++) {
+            fromStack = fromStack.Pop(out var crate);
+            tempStack.Push(crate);    
+        }
+
+        var toStack = stacks[move.To];
+        while (tempStack.Count > 0) {
+            var crate = tempStack.Pop();
+            toStack = toStack.Push(crate);
+        }
 
         return stacks
             .SetItem(move.From, fromStack)
