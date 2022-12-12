6aa75e61fc8e11dd6f25d42c475a571c980ff985
Greg Tan <greg.tan@firstmode.com>
2022-12-12 23:39:49 +0800

    Day 12 part 2.

--- aoc12/PathFinder.cs
+++ aoc12/PathFinder.cs
@@ -1,7 +1,7 @@
 using System.Collections.Immutable;
 
 public static class PathFinder {
-    public static ImmutableArray<Point> FindPath(Point start, Point end, ImmutableArray<ImmutableArray<char>> grid) {
+    public static ImmutableArray<Point> FindPath(Point start, Point end, int maxLength, ImmutableArray<ImmutableArray<char>> grid) {
         var queue = new Queue<ImmutableArray<Point>>();
         queue.Enqueue(ImmutableArray.Create<Point>(start));
         var visited = ImmutableHashSet<Point>.Empty;
@@ -18,6 +18,10 @@ public static class PathFinder {
                 continue;
             }
 
+            if (path.Count() >= maxLength) {
+                continue;
+            }
+
             visited = visited.Add(current);
             foreach(var nextMove in FindNextMoves(current, grid)) {
                 queue.Enqueue(path.Add(nextMove));
@@ -25,6 +29,34 @@ public static class PathFinder {
         }
 
         return ImmutableArray<Point>.Empty;
+    }
+
+    public static ImmutableArray<Point> FindShortestPathFromLowestPoints(Point end, ImmutableArray<ImmutableArray<char>> grid) {
+        var startPoints = FindOrderedStartPoints(end, grid);
+        var minLength = int.MaxValue;
+        var bestPath = ImmutableArray<Point>.Empty;
+
+        foreach (var start in startPoints) {
+            var path = FindPath(start, end, minLength, grid);
+            if (path.Count() > 0 && path.Count() < minLength) {
+                minLength = path.Count();
+                bestPath = path;
+            }
+        }
+
+        return bestPath;
+    }
+
+    private static ImmutableArray<Point> FindOrderedStartPoints(Point end, ImmutableArray<ImmutableArray<char>> grid) {
+        return Enumerable.Range(0, grid.Count())
+            .SelectMany(y => Enumerable.Range(0, grid[y].Count()).Select(x => new Point(x, y)))
+            .Where(p => grid[p.Y][p.X] == 'a')
+            .OrderBy(s => ManhattanDistance(s, end))
+            .ToImmutableArray();
+    }
+
+    private static int ManhattanDistance(Point a, Point b) {
+        return Math.Abs(a.X - b.X) + Math.Abs(a.Y - b.Y);
     }
 
     private static ImmutableArray<Point> FindNextMoves(Point current, ImmutableArray<ImmutableArray<char>> grid) {
--- aoc12/Program.cs
+++ aoc12/Program.cs
@@ -2,7 +2,12 @@
     public static void Main(string[] args) {
         var (start, end, grid) = GridParser.Parse(args[0]);
 
-        var path = PathFinder.FindPath(start, end, grid);
-        Console.WriteLine(path.Count() -1 ); // -1 to exclude start point
+        var path = PathFinder.FindPath(start, end, int.MaxValue, grid);
+        var numSteps = path.Count() - 1; // -1 to exclude start point
+        Console.WriteLine($"Min steps: {numSteps}"); 
+
+        var shortestPathFromAnyLowPoint = PathFinder.FindShortestPathFromLowestPoints(end, grid);
+        var numStepsFromAnyLowPoint = shortestPathFromAnyLowPoint.Count() - 1;
+        Console.WriteLine($"Min steps from any low point: {numStepsFromAnyLowPoint}");
     }
 }
